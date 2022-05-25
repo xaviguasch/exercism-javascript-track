51523bd9f834072ff37d84e902bb074d35eb0d3f
xaviguasch <xaviguasch@gmail.com>
2022-05-25 11:11:53 +0200

    solved task 4 and 5 and finished previous exercise

--- 013-lasagna-master/lasagna-master.js
+++ 013-lasagna-master/lasagna-master.js
@@ -43,6 +43,20 @@ function quantities(layersArr) {
   }
 }
 
+function addSecretIngredient(fList, personalList) {
+  personalList.push(fList[fList.length - 1])
+}
+
+function scaleRecipe(recipe, portions = 2) {
+  const newRecipe = {}
+
+  for (const key in recipe) {
+    newRecipe[key] = recipe[key] * (portions / 2)
+  }
+
+  return newRecipe
+}
+
 // ==================
 // console.log(cookingStatus(12))
 // console.log(cookingStatus())
@@ -51,4 +65,20 @@ function quantities(layersArr) {
 // console.log(preparationTime(layers, 3))
 // console.log(preparationTime(layers))
 // ==================
-console.log(quantities(['sauce', 'noodles', 'sauce', 'meat', 'mozzarella', 'noodles']))
+// console.log(quantities(['sauce', 'noodles', 'sauce', 'meat', 'mozzarella', 'noodles']))
+// ==================
+// const friendsList = ['noodles', 'sauce', 'mozzarella', 'kampot pepper']
+// const myList = ['noodles', 'meat', 'sauce', 'mozzarella']
+
+// console.log(addSecretIngredient(friendsList, myList))
+// console.log(myList)
+// ==================
+const recipe = {
+  noodles: 200,
+  sauce: 0.5,
+  mozzarella: 1,
+  meat: 100,
+}
+
+console.log(scaleRecipe(recipe, 4))
+console.log(scaleRecipe(recipe))
