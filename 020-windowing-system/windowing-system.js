5f1da471c21bc6949d7f785c127091671e9310b2
xaviguasch <xaviguasch@gmail.com>
2022-05-27 18:25:05 +0200

    solved task 4 of previous exercise

--- 020-windowing-system/windowing-system.js
+++ 020-windowing-system/windowing-system.js
@@ -32,11 +32,33 @@ Position.prototype.move = function (newX, newY) {
 
 // TASK 3
 
+// class ProgramWindow {
+//   constructor() {
+//     this.screenSize = new Size(800, 600)
+//     this.size = new Size()
+//     this.position = new Position()
+//   }
+// }
+
+// TASK 4
+
 class ProgramWindow {
   constructor() {
     this.screenSize = new Size(800, 600)
     this.size = new Size()
     this.position = new Position()
+  }
+
+  resize(newSize) {
+    const targetWidth = newSize.width > 1 ? newSize.width : 1
+    const maxWidth = this.screenSize.width - this.position.x
+    const newWidth = maxWidth > targetWidth ? targetWidth : maxWidth
+
+    const targetHeight = newSize.height > 1 ? newSize.height : 1
+    const maxHeight = this.screenSize.height - this.position.y
+    const newHeight = maxHeight > targetHeight ? targetHeight : maxHeight
+
+    this.size.resize(newWidth, newHeight)
   }
 }
 
@@ -65,14 +87,22 @@ class ProgramWindow {
 // ==================
 // 3 -
 
-const programWindow = new ProgramWindow()
-console.log(programWindow.screenSize.width)
-console.log(programWindow.screenSize.height)
-console.log(programWindow.size)
-console.log(programWindow.position)
+// const programWindow = new ProgramWindow()
+// console.log(programWindow.screenSize.width)
+// console.log(programWindow.screenSize.height)
+// console.log(programWindow.size)
+// console.log(programWindow.position)
 
 // ==================
 // 4 -
+
+const programWindow = new ProgramWindow()
+
+const newSize = new Size(600, 400)
+programWindow.resize(newSize)
+console.log(programWindow.size.width)
+console.log(programWindow.size.height)
+
 // ==================
 // 5 -
 // ==================
