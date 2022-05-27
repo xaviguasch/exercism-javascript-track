b840f6e2d801353d4944c56cea586abe8fb20533
xaviguasch <xaviguasch@gmail.com>
2022-05-27 16:36:40 +0200

    solved task 1 of previous exercise

--- 020-windowing-system/windowing-system.js
+++ 020-windowing-system/windowing-system.js
@@ -5,3 +5,37 @@
  * exercise in this file. Do not forget to export the entities
  * you defined so they are available for the tests.
  */
+
+// TASK 1
+
+function Size(width = 80, height = 60) {
+  this.width = width
+  this.height = height
+}
+
+Size.prototype.resize = function (newWidth, newHeight) {
+  this.width = newWidth
+  this.height = newHeight
+}
+
+// ==================
+// 1 -
+const size = new Size(1080, 764)
+console.log(size.width)
+console.log(size.height)
+
+size.resize(1920, 1080)
+console.log(size.width)
+console.log(size.height)
+
+// ==================
+// 2 -
+// ==================
+// 3 -
+// ==================
+// 4 -
+// ==================
+// 5 -
+// ==================
+// 6 -
+// ==================
