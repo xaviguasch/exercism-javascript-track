cd1465d73f509ddbc6a6e3a82377927a06e99175
xaviguasch <xaviguasch@gmail.com>
2022-05-27 17:53:49 +0200

    solved task 2 of previous exercise

--- 020-windowing-system/windowing-system.js
+++ 020-windowing-system/windowing-system.js
@@ -8,28 +8,50 @@
 
 // TASK 1
 
-function Size(width = 80, height = 60) {
-  this.width = width
-  this.height = height
+// function Size(width = 80, height = 60) {
+//   this.width = width
+//   this.height = height
+// }
+
+// Size.prototype.resize = function (newWidth, newHeight) {
+//   this.width = newWidth
+//   this.height = newHeight
+// }
+
+// TASK 2
+
+function Position(x = 0, y = 0) {
+  this.x = x
+  this.y = y
 }
 
-Size.prototype.resize = function (newWidth, newHeight) {
-  this.width = newWidth
-  this.height = newHeight
+Position.prototype.move = function (newX, newY) {
+  this.x = newX
+  this.y = newY
 }
 
 // ==================
 // 1 -
-const size = new Size(1080, 764)
-console.log(size.width)
-console.log(size.height)
 
-size.resize(1920, 1080)
-console.log(size.width)
-console.log(size.height)
+// const size = new Size(1080, 764)
+// console.log(size.width)
+// console.log(size.height)
+
+// size.resize(1920, 1080)
+// console.log(size.width)
+// console.log(size.height)
 
 // ==================
 // 2 -
+
+const point = new Position()
+console.log(point.x)
+console.log(point.y)
+
+point.move(100, 200)
+console.log(point.x)
+console.log(point.y)
+
 // ==================
 // 3 -
 // ==================
