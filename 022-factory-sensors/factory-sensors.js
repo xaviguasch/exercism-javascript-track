c3312d54d220d8839278f92fbbb975a4664a9c8a
xaviguasch <xaviguasch@gmail.com>
2022-05-28 17:50:51 +0200

    added 022-factory-sensors exercise teaching Errors and Inheritance concepts

---
+++ 022-factory-sensors/factory-sensors.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+class ArgumentError extends Error {}
+
+class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`)
+    this.temperature = temperature
+  }
+}
+
+/**
+ * Check if the humidity level is not too high.
+ *
+ * @param {number} humidityPercentage
+ * @throws {Error}
+ */
+function checkHumidityLevel(humidityPercentage) {
+  throw new Error('Implement the checkHumidity function')
+}
+
+/**
+ * Check if the temperature is not too high.
+ *
+ * @param {number|null} temperature
+ * @throws {ArgumentError|OverheatingError}
+ */
+function reportOverheating(temperature) {
+  throw new Error('Implement the reportOverheating function')
+}
+
+/**
+ *  Triggers the needed action depending on the result of the machine check.
+ *
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
+ */
+function monitorTheMachine(actions) {
+  throw new Error('Implement the monitorTheMachine function')
+}
