278790682cb769bc5b944a88ef104304c916f4b4
xaviguasch <xaviguasch@gmail.com>
2022-05-28 19:10:09 +0200

    added 023-ozons-playlist exercise Sets practice

---
+++ 023-ozons-playlist/ozons-playlist.js
@@ -0,0 +1,69 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+function removeDuplicates(playlist) {
+  throw new Error('Please implement the removeDuplicates function')
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+function hasTrack(playlist, track) {
+  throw new Error('Please implement the hasTrack function')
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+function addTrack(playlist, track) {
+  throw new Error('Please implement the addTrack function')
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+function deleteTrack(playlist, track) {
+  throw new Error('Please implement the deleteTrack function')
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+function listArtists(playlist) {
+  throw new Error('Please implement the listArtists function')
+}
+
+// ==================
+// 1 -
+
+// ==================
+// 2 -
+
+// ==================
+// 3 -
+
+// ==================
