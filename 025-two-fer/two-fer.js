cabd028a9cc5ca187ed6311a945d379128acd214
xaviguasch <xaviguasch@gmail.com>
2022-05-30 09:33:46 +0200

    solved task1 of previous exercise and completed it

---
+++ 025-two-fer/two-fer.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`
+}
+
+// ==================
+console.log(twoFer('Alice'))
+console.log(twoFer('Bob'))
+console.log(twoFer())
+console.log(twoFer('Zaphod'))
