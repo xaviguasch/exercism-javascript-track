8f5160886699f2d97b28e3c97a2261cf7d2a695e
xaviguasch <xaviguasch@gmail.com>
2022-05-30 09:38:48 +0200

    added 026-reistor-color exercise

---
+++ 026-resistor-color/resistor-color.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const colorCode = () => {
+  throw new Error('Remove this statement and implement this function')
+}
+
+const COLORS = undefined
+
+// ==================
+console.log(colorCode('black'))
+console.log(colorCode('white'))
+console.log(colorCode('orange'))
