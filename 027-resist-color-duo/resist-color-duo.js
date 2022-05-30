af6d5b3c67c730757a9f1b9278edd0fb3e81efae
xaviguasch <xaviguasch@gmail.com>
2022-05-30 10:01:13 +0200

    solved task 1 from previous exercise and completed it

--- 027-resist-color-duo/resist-color-duo.js
+++ 027-resist-color-duo/resist-color-duo.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function')
+const decodedValue = (colorsArr) => {
+  const COLORS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  }
+
+  const [color1, color2] = colorsArr
+
+  return COLORS[color1].toString() + COLORS[color2].toString()
 }
+
+// ==================
+console.log(decodedValue(['brown', 'black']))
+console.log(decodedValue(['blue', 'grey']))
